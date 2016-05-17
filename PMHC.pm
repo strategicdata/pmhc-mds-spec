e96f0659959960a0d5c8b51453fbd43e1203e81c
Adam Clarke <adam.clarke@strategicdata.com.au>
2016-05-17 15:39:13 +1000

    Add all ddict files to PMHC dir

---
+++ PMHC.pm
@@ -0,0 +1,10 @@
+package DDict::PMHC;
+# Data Dictionary for the PMHC file
+use strict;
+use warnings;
+use base 'DDict';
+
+my $CLASS = __PACKAGE__;
+$CLASS->dict_file_name('pmhc-dict.yaml');
+
+1;
