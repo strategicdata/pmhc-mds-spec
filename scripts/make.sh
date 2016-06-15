47465031b8c87cf9b6aa053c7f168f63e339f3c6
Adam Clarke <adam.clarke@strategicdata.com.au>
2016-06-15 16:48:39 +1000

    Update docs

--- scripts/make.sh
+++ scripts/make.sh
@@ -36,7 +36,7 @@ pushd .
 
 cd doc
 
-curl -s http://www.gliffy.com/go/publish/image/10751755/L.png > figures/data-model.png
+curl -s https://www.gliffy.com/go/publish/image/10751755/L.png > figures/data-model.png
 
 docker run -ti -v `pwd`:/mnt/workdir stratdat/sphinx:production make $1
 
