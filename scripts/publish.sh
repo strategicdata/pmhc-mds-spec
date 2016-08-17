8ed9baf3a1c1515b3934218599b6b3edcb85c425
Adam Clarke <adam.clarke@strategicdata.com.au>
2016-08-17 16:33:01 +1000

    Use autodeploy on docker cloud

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 docker build -t stratdat/docs-pmhc-mds doc \
-  && docker push stratdat/docs-pmhc-mds \
-  && curl -I -X POST https://cloud.docker.com//api/app/v1/service/1585501e-ca0c-4187-85d6-fb0348ec8e74/trigger/36734c4f-8606-4f6c-9dc5-337194fedbaf/call/
+  && docker push stratdat/docs-pmhc-mds
