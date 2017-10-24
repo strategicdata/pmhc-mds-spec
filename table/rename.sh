f9647947aef50d9216a3ffd257a9b63af2c82116
Adam Clarke <adam.clarke@strategicdata.com.au>
2017-10-24 16:00:21 +1100

    Remove unneeded utility script

--- table/rename.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-perl -e '
-FILE:for $file (@ARGV){
-        ($new_name = $file) =~ s/.txt/.rst/;
-        next FILE if -e $new_name;
-        rename $file => $new_name;
-}' *.txt
