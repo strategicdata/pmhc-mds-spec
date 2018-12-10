cbb75f666c8d0a60925fda2c83d826d18eada424
Adam Clarke <adamc@strategicdata.com.au>
2018-12-10 14:09:38 +1100

    Build PDF locally

--- scripts/make.sh
+++ scripts/make.sh
@@ -11,6 +11,7 @@ rm -rf doc/_static/pmhcmds-spec-meta.zip
 # copy new zip to data-specification folder
 mv pmhcmds-spec-meta.zip doc/_static/
 
+docker pull docker.sdlocal.net/csvw/metadata2rst
 docker run --rm -v `pwd`:/mnt/cwd docker.sdlocal.net/csvw/metadata2rst \
   --meta=pmhc-metadata.json
 
@@ -21,12 +22,6 @@ cd doc
 rm -rf data-specification/_data build
 cp -rf ../data data-specification/_data
 
-if [ -z $1 ]; then
-  ARG1=html
-else
-  ARG1=$1
-fi
-
 GIT_VERSION=$(git describe --tags --always)
 
 current_dir=`pwd`
@@ -35,6 +30,13 @@ echo "PWD=$current_dir"
 
 pwd
 
-docker run -e GIT_VERSION -v `pwd`:/mnt/workdir stratdat/sphinx:production make $ARG1
+docker pull stratdat/sphinx:production
+docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir stratdat/sphinx:production make html singlehtml
 
 popd
+
+docker pull stratdat/sphinx-html2pdf:production
+docker run -e GIT_VERSION -v `pwd`:/mnt/workdir stratdat/sphinx-html2pdf:production \
+  /mnt/workdir/scripts/make-pdf.pl \
+  --spec-name "PMHC" \
+  --doc-dir   "/mnt/workdir/doc"
