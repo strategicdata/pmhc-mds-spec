b27f485716298e1f0e4f99a6affb12bbef12c481
Adam Clarke <adamc@strategicdata.com.au>
2018-12-10 18:06:50 +1100

    Downscale images for PDF

--- scripts/make.sh
+++ scripts/make.sh
@@ -37,6 +37,14 @@ docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
 popd
 
 docker pull stratdat/sphinx-html2pdf:production
+
+echo "Optimising images"
+docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
+  --workdir /mnt/workdir/doc/build/singlehtml/_images \
+  stratdat/sphinx-html2pdf:production \
+  find . -name *.png -exec pngquant --force --output {} 8 {} \;
+
+echo "Building PDF"
 docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
   stratdat/sphinx-html2pdf:production \
   /mnt/workdir/scripts/make-pdf.pl \
